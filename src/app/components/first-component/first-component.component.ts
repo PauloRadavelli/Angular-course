a4f673812dc92859086a1590f3155d2a5807fac3
paulosradavelli <paulosradavelli@gmail.com>
2023-02-08 22:00:51 -0300

    Create first component and use interpolation

---
+++ src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.scss']
+})
+export class FirstComponentComponent {
+  name: string = "Paulo"
+  age: number = 22
+  job: string = "Programador"
+}
